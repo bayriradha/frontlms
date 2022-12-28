0d3bb8e261d90fa2e419ed329f0aadd7ca740e78
bayriradha <102705199+bayriradha@users.noreply.github.com>
2022-12-28 14:59:47 +0530

    Update environment.prod.ts
    Load balancer url added for connectivity

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://lms-api.osacad.com'
-  apiUrl: 'https://dev-backend.konamars.com'
+  apiUrl: 'http:a118e524eb2ad42d1a676f42b8d117eb-1517106927.ap-south-1.elb.amazonaws.com'
 };
